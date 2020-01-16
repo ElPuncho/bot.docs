8953aa202280b59d97545c055a19436323e98112
Gab Ra <gabrielrashidi@web.de>
2020-01-16 14:54:15 +0100

    Beta-003:kmeans\n updated file name, removed preprocessor import, updated variable names(X, text)

---
+++ chatbot-beta-001/py_files/kmeans.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.cluster import KMeans
+import sys
+
+class Kmeans:
+    def __init__(self, tfidfMatrix, sentences):
+        self.tfidfMatrix = tfidfMatrix
+        self.sentences = sentences
+
+    def generateModel(self):
+        kmeans = KMeans(n_clusters=int(len(self.sentences)/3))
+        kmeans.fit_predict(self.tfidfMatrix)
+        labels = kmeans.predict(self.tfidfMatrix)
+        kmeans.cluster_centers_
+        return labels
+
+    def getRelevantSentences(self):
+        relevantSentences = str()
+        labels = self.generateModel()
+        idx = np.where(labels == labels[-1])
+        for i in idx[0][:-1]:
+            relevantSentences += self.sentences[int(i)] + '\n'
+        return relevantSentences
