8e8635510391e34746dc32c52ac8af2039091b59
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-01-23 18:50:28 +0100

    Beta-017: pep8 check controllertest
    Beta-017: pep8 check controllertest

--- chatbot-beta-001/tests/controllertest.py
+++ chatbot-beta-001/tests/controllertest.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import unittest, os
+import unittest
+import os
 import py_files.filesystemhandler as fsh
 import py_files.controller as cont
+
 
 class TestController(unittest.TestCase):
     def testInit(self):
@@ -12,22 +14,24 @@ class TestController(unittest.TestCase):
         self.assertEqual(con.filterObject, None)
         self.assertEqual(con.crawlerObject, None)
         self.assertIs(type(con.fileHandler), fsh.FileSystemHandler)
-        
+
     def testInitSession(self):
         query = "python:math:how to use sine"
         con = cont.ControlManager(query)
         con.initSession()
         self.assertTrue(os.path.isdir(con.fileHandler.getRootDirectory()))
-        
+
     def testCrawlQuery(self):
         query = "python:math:how to use sine"
         con = cont.ControlManager(query)
         con.initSession()
         con.crawlQuery()
-        self.assertTrue(con.fileHandler.getSearchFilePath("python", "math.txt"))
-        fileTest = open(con.fileHandler.getSearchFilePath("python", "math.txt")).read()
-        self.assertTrue("sin" in fileTest and "cos" in fileTest and "<html>" not in fileTest)
-    
+        filePath = con.fileHandler.getSearchFilePath("python", "math.txt")
+        self.assertTrue(filePath)
+        fileTest = open(filePath).read()
+        self.assertTrue("<html>" not in fileTest)
+        self.assertTrue("sin" in fileTest and "cos" in fileTest)
+
     def testExecuteFiltering(self):
         query = "python:math:how to use sine"
         con = cont.ControlManager(query)
@@ -35,18 +39,18 @@ class TestController(unittest.TestCase):
         con.crawlQuery()
         result = con.executeFiltering()
         self.assertTrue("sin(x)" in result)
-        
+
     def testCloseSession(self):
         query = "python:math:how to use sine"
         con = cont.ControlManager(query)
         con.initSession()
         con.crawlQuery()
-        result = con.executeFiltering()
+        con.executeFiltering()
         con.closeSession()
         rootDir = con.fileHandler.getRootDirectory()
-        self.assertTrue(os.path.exists(rootDir) and os.path.isdir(rootDir) and not os.listdir(rootDir))
-        
+        self.assertTrue(os.path.exists(rootDir))
+        self.assertTrue(os.path.isdir(rootDir) and not os.listdir(rootDir))
+
 
 if __name__ == "__main__":
     unittest.main()
-
