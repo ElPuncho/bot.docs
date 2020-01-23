e534a5aab899d3683de20fc901c77fbb11d710bf
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-01-23 18:19:15 +0100

    Beta-006: pep8 check controller
    pep8 check controller

--- chatbot-beta-001/py_files/controller.py
+++ chatbot-beta-001/py_files/controller.py
@@ -4,27 +4,35 @@ import py_files.filesystemhandler as fsh
 import py_files.crawler as cwl
 import py_files.collaborativeFiltering as cfl
 
+
 class ControlManager:
     def __init__(self, query):
         self.query = query
         self.filterObject = None
         self.crawlerObject = None
         self.fileHandler = fsh.FileSystemHandler()
-    
+
     def initSession(self):
         self.fileHandler.createRootDirectory()
-        
+
     def closeSession(self):
-        self.fileHandler.deleteSessionTempFiles(self.fileHandler.getRootDirectory())
-        
+        rootD = self.fileHandler.getRootDirectory()
+        self.fileHandler.deleteSessionTempFiles(rootD)
+
     def crawlQuery(self):
         self.crawlerObject = cwl.Crawler(self.query)
-        self.crawlerObject.writeTextToFile(self.crawlerObject.getTextFromWebsite(self.crawlerObject.getLibUrl()))
-        
+        lib = self.crawlerObject.getLibUrl()
+        text = self.crawlerObject.getTextFromWebsite(lib)
+        self.crawlerObject.writeTextToFile(text)
+
     def executeFiltering(self):
-        self.filterObject = cfl.CollaborativeFiltering(self.fileHandler.getRootDirectory() + self.fileHandler.getSlashForOsVersion(), self.query.split(":")[2])
+        file = self.fileHandler.getRootDirectory()
+        file += self.fileHandler.getSlashForOsVersion()
+        searchString = self.query.split(":")[2]
+        self.filterObject = cfl.CollaborativeFiltering(file, searchString)
         return self.filterObject.generateResponse()
-        
+
+
 if __name__ == "__main__":
     con = ControlManager("c++:vector:create vector")
     con.initSession()
