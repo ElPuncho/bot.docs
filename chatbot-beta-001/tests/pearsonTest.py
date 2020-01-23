bdbe6999e990b3a2bf9076ff95e904b62075c06f
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-01-23 19:30:26 +0100

    Beta-25: pep8 check pearsonTest
    Beta-25: pep8 check pearsonTest

--- chatbot-beta-001/tests/pearsonTest.py
+++ chatbot-beta-001/tests/pearsonTest.py
@@ -1,14 +1,13 @@
 import sys
-sys.path.append('..')
 import unittest
-import numpy as np
-from sklearn.cluster import KMeans
 import py_files.pearson as p
 import py_files.preprocessor as pre
+sys.path.append('..')
 
 
 class pearsonTest(unittest.TestCase):
-    preprocessor = pre.Preprocessor('data/', 'how to compute base 10 logarithm')
+    query = 'how to compute base 10 logarithm'
+    preprocessor = pre.Preprocessor('data/', query)
     X = preprocessor.vectoriseData()
     sentences = preprocessor.fetchData().split('.')
     pearson = p.Pearson(X, sentences)
@@ -17,16 +16,19 @@ class pearsonTest(unittest.TestCase):
         self.assertIsNotNone(self.pearson.tfidfMatrix)
         self.assertIsNotNone(self.pearson.pearsonCoeffs)
         self.assertIsNotNone(self.pearson.sentences)
-        self.assertEqual(self.pearson.tfidfMatrix.shape[0], len(self.pearson.sentences))
+        lenPearson = len(self.pearson.sentences)
+        self.assertEqual(self.pearson.tfidfMatrix.shape[0], lenPearson)
 
     def testGetPearsonCoeffsOfUserInputAndData(self):
         self.assertIsNotNone(self.pearson.getPearsonCoeffsOfUserInputAndData())
-        self.assertEqual(self.pearson.tfidfMatrix.shape[0], len(self.pearson.getPearsonCoeffsOfUserInputAndData()))
+        lenPearson = len(self.pearson.getPearsonCoeffsOfUserInputAndData())
+        self.assertEqual(self.pearson.tfidfMatrix.shape[0], lenPearson)
 
     def testGetIndexOfTopKMatches(self):
         topKMatches = 3
-        self.assertIsNotNone(self.pearson.getIndexOfTopKMatches(topKMatches))
-        self.assertEqual(len(self.pearson.getIndexOfTopKMatches(topKMatches)), topKMatches)
+        index = self.pearson.getIndexOfTopKMatches(topKMatches)
+        self.assertIsNotNone(index)
+        self.assertEqual(len(index), topKMatches)
 
     def testGenerateResponse(self):
         self.assertIsNotNone(self.pearson.generateResponse())
