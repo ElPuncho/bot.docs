544d968913c3a464bc25f180b3fe33328ddebb60
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-01-23 18:44:42 +0100

    Beta-016: pep8 check advancedfiltersystem
    Beta-016: pep8 check advancedfiltersystem

--- chatbot-beta-001/py_files/advancedtextfiltersystem.py
+++ chatbot-beta-001/py_files/advancedtextfiltersystem.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re, json
+import re
+import json
+
 
 class AdvancedTextFilterSystem:
     def __init__(self, filePath, savePath, query):
@@ -9,28 +11,28 @@ class AdvancedTextFilterSystem:
         self.query = query
         self.functions = list()
         self.filterData = json.load(filePath)
-        
+
     def getFilter(self):
         return self.filterData[self.query.split(":")[0]]
-    
+
     def filterFunctionsFromText(self):
         searchString = self.getFilter()
         for line in self.fileLines:
             if re.search(searchString, line):
                 self.functions.append(line)
-    
+
     def writeFunctionsAndExplanationToFile(self, file):
         for i in range(1, len(self.functions)-1):
-            funcIndex = self.fileLines.index(self.functions[i])     
+            funcIndex = self.fileLines.index(self.functions[i])
             while True:
                 if funcIndex == self.fileLines.index(self.functions[i+1]):
                     break
                 file.write(self.fileLines[funcIndex])
                 funcIndex += 1
             file.write("\n")
-            
+
     def executeFitering(self):
-        file = open(self.savePath, "w", encoding = "utf8")
+        file = open(self.savePath, "w", encoding="utf8")
         self.filterFunctionsFromText()
         self.writeFunctionsAndExplanationToFile(file)
         file.close()
