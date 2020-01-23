d3f4d7e675544f4a4ed79d75e97f934d728354c9
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-01-23 19:27:06 +0100

    Beta-024: pep8 check kmeansTest
    Beta-024: pep8 check kmeansTest

--- chatbot-beta-001/tests/kmeansTest.py
+++ chatbot-beta-001/tests/kmeansTest.py
@@ -1,13 +1,13 @@
 import sys
-sys.path.append('..')
 import unittest
-import numpy as np
-from sklearn.cluster import KMeans
 import py_files.kmeans as km
 import py_files.preprocessor as pre
+sys.path.append('..')
+
 
 class kmeansTest(unittest.TestCase):
-    preprocessor = pre.Preprocessor('data/', 'how to compute base 10 logarithm')
+    query = 'how to compute base 10 logarithm'
+    preprocessor = pre.Preprocessor('data/', query)
     X = preprocessor.vectoriseData()
     sentences = preprocessor.fetchData().split('.')
     kmeans = km.Kmeans(X, sentences)
@@ -15,11 +15,13 @@ class kmeansTest(unittest.TestCase):
     def testInit(self):
         self.assertIsNotNone(self.kmeans.tfidfMatrix)
         self.assertIsNotNone(self.kmeans.sentences)
-        self.assertEqual(self.kmeans.tfidfMatrix.shape[0], len(self.kmeans.sentences))
+        lenKMeans = len(self.kmeans.sentences)
+        self.assertEqual(self.kmeans.tfidfMatrix.shape[0], lenKMeans)
 
     def testGenerateModel(self):
         self.assertIsNotNone(self.kmeans.generateModel())
-        self.assertEqual(self.kmeans.tfidfMatrix.shape[0], len(self.kmeans.generateModel()))
+        lenKMeans = len(self.kmeans.generateModel())
+        self.assertEqual(self.kmeans.tfidfMatrix.shape[0], lenKMeans)
 
     def testGetRelevantSentences(self):
         self.assertIsNotNone(self.kmeans.getRelevantSentences())
