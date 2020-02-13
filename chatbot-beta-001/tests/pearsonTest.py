7bc08f4fec53b8c881fa279392fb8ac5190fd62e
Gab Ra <gabrielrashidi@web.de>
2020-02-13 14:39:21 +0100

    beta:pearsonTest updated test for testInit testGetPearsonCoeffsOfUserInputAndData

--- chatbot-beta-001/tests/pearsonTest.py
+++ chatbot-beta-001/tests/pearsonTest.py
@@ -1,15 +1,16 @@
 import sys
+sys.path.append('..')
 import unittest
 import py_files.pearson as p
 import py_files.preprocessor as pre
-sys.path.append('..')
+
 
 
 class pearsonTest(unittest.TestCase):
     query = 'how to compute base 10 logarithm'
-    preprocessor = pre.Preprocessor('data/', query)
+    preprocessor = pre.Preprocessor('../tests/data/math/numpyMathFunctions.txt', query)
     X = preprocessor.vectoriseData()
-    sentences = preprocessor.fetchData().split('.')
+    sentences = preprocessor.removeDuplicateLines()
     pearson = p.Pearson(X, sentences)
 
     def testInit(self):
@@ -17,12 +18,12 @@ class pearsonTest(unittest.TestCase):
         self.assertIsNotNone(self.pearson.pearsonCoeffs)
         self.assertIsNotNone(self.pearson.sentences)
         lenPearson = len(self.pearson.sentences)
-        self.assertEqual(self.pearson.tfidfMatrix.shape[0], lenPearson)
+        self.assertEqual(self.pearson.tfidfMatrix[:-1].shape[0], lenPearson)
 
     def testGetPearsonCoeffsOfUserInputAndData(self):
         self.assertIsNotNone(self.pearson.getPearsonCoeffsOfUserInputAndData())
         lenPearson = len(self.pearson.getPearsonCoeffsOfUserInputAndData())
-        self.assertEqual(self.pearson.tfidfMatrix.shape[0], lenPearson)
+        self.assertEqual(self.pearson.tfidfMatrix[:-1].shape[0], lenPearson)
 
     def testGetIndexOfTopKMatches(self):
         topKMatches = 3
