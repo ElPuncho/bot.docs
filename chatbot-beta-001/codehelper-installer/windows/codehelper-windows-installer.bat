ced5074642a046be430f377042aadf4307112a64
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-02-18 18:50:48 +0100

    beta: windows installer
    -fixed issues with the executimg of commands
    -new problem: vscode needs to pc to restart after intsallitaion
    -solution for new problem: divide the installation procees in two parts

--- chatbot-beta-001/codehelper-installer/windows/codehelper-windows-installer.bat
+++ chatbot-beta-001/codehelper-installer/windows/codehelper-windows-installer.bat
@@ -4,61 +4,38 @@ echo "Bot Docs B 'CodeHelper' Installer Windows 64 Bit"
 echo
 
 echo "Installing Python3..."
-CALL :installPython3
+CD python-3.8.1-amd64
+START /W python-3.8.1-amd64.exe
+CD ..
 echo "python3 installed"
 echo
 
 echo "Installing Python3 Libraries..."
-CALL :installPythonLibraries
+SET libs=(requests sys os beautifulsoup4 numpy sklearn2 scikit-learn scipy nltk)
+(FOR %%a in %libs% DO (
+	echo "Installing Python Library %%a"
+	py -3 -m pip install %%a
+))
 echo "Installed all Python3 Libraries"
 echo
 
 echo "Installing VS Code..."
-CALL :installVsCode
+CD vscode
+START /W VSCodeUserSetup-x64-1.42.1.exe
+CD ..
 echo "Installed VS code"
 echo
 
 echo "Installing Node JS..."
-CALL :installNodeJs
+CD nodejs
+START /W node-v12.16.0-x64.msi
+CD ..
 echo "Installed Node JS"
 echo
 
 echo "Installing VS Code Extenstion 'CodeHelper'..."
-CAll :installVsCodeExtenstion
+code --install-extension codehelper\codehelper-0.0.1.vsix
+code
 echo "Installed VS Code Extenstion 'CodeHelper'"
 
 PAUSE
-EXIT /B %ERRORLEVEL% 
-
-:installPython3
-CD python-3.8.1-amd64
-START /W "python-3.8.1-amd64.exe"
-CD ..
-EXIT /B 0
-
-:installPythonLibraries
-SET libs = requests sys os beautifulsoup4 numpy sklearn2 scikit-learn scipy nltk
-(FOR %%lib in (%libs)
-DO (
-	echo "Installing Python Library %%lib"
-	py -3 -m pip install %%lib
-))
-EXIT /B 0
-
-:installVsCode
-CD vscode
-START /W "VSCodeUserSetup-x64-1.42.1.exe"
-CD ..
-EXIT /B 0
-
-:installNodeJs
-CD nodejs
-START /W "node-v12.16.0-x64.msi"
-CD ..
-EXIT /B 0
-
-
-:installVsCodeExtenstion
-code --install-extension codehelper\codehelper-0.0.1.vsix
-code
-EXIT /B 0
