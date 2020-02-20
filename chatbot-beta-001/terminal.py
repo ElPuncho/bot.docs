9684a60b8a671a3a0492a5ff72bfeb041a4ba7b9
Philipp Wiedemann <Philipp.Wiedemann@stud.uni-heidelberg.de>
2020-02-20 19:22:13 +0100

    beta: terminal
    minor changes

--- chatbot-beta-001/terminal.py
+++ chatbot-beta-001/terminal.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         c.initSession()
         c.crawlQuery()
         result = c.executeFiltering()
-        c.closeSession()
+        #c.closeSession()
         print(result)
